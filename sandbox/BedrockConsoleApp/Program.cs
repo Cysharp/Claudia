50450daa1251b7fbf175e56098dfe988d147af1c
neuecc <ils@neue.cc>
2024-03-18 16:06:34 +0900

    done Bedrock CreateAsync

--- sandbox/BedrockConsoleApp/Program.cs
+++ sandbox/BedrockConsoleApp/Program.cs
@@ -1,56 +1,18 @@
 ﻿using Amazon;
 using Amazon.BedrockRuntime;
-using Amazon.BedrockRuntime.Model;
-using Amazon.Util;
 using Claudia;
-using System.Text;
-using System.Text.Json;
-using System.Text.Json.Nodes;
-using System.Text.Json.Serialization;
-using System.Text.Json.Serialization.Metadata;
 
 // credentials is your own
 AWSConfigs.AWSProfileName = "";
 
 var bedrock = new AmazonBedrockRuntimeClient(RegionEndpoint.USEast1);
+var anthropic = bedrock.UseAnthropic("anthropic.claude-3-sonnet-20240229-v1:0");
 
-
-var input = """
-        What time is it in Seattle and Tokyo?
-        Incidentally multiply 1,984,135 by 9,343,116.
-""";
-var payload = new JsonObject()
-    {
-        { "anthropic_version", "bedrock-2023-05-31" },
-        { "max_tokens", 1000 },
-        { "messages", new JsonArray(new JsonObject{
-            { "role", "user" },
-            { "content", new JsonArray(new JsonObject{
-                { "type", "text" },
-                { "text", input }
-            })}
-        })
-        }
-    }.ToJsonString();
-
-
-
-
-
-
-
-var response = await bedrock.InvokeModelAsync("anthropic.claude-3-sonnet-20240229-v1:0", new()
+var response = await anthropic.Messages.CreateAsync(new()
 {
     Model = "bedrock-2023-05-31",
     MaxTokens = 1024,
     Messages = [new() { Role = "user", Content = "Hello, Claude" }]
 });
 
-if (response.HttpStatusCode != System.Net.HttpStatusCode.OK)
-    throw new HttpRequestException("Request Failed", null, response.HttpStatusCode);
-
-var resBody = await JsonNode.ParseAsync(response.Body);
-Console.WriteLine(resBody);
-
-
-
+Console.WriteLine(response);
--- src/Claudia.Bedrock/BedrockExtensions.cs
+++ src/Claudia.Bedrock/BedrockExtensions.cs
@@ -4,8 +4,30 @@ using System.Text.Json;
 
 namespace Claudia;
 
+public class BedrockAnthropicClient(AmazonBedrockRuntimeClient client, string modelId) : IMessages
+{
+    public IMessages Messages => this;
+
+    // currently overrideOptions is not yet supported.
+    async Task<MessageResponse> IMessages.CreateAsync(MessageRequest request, RequestOptions? overrideOptions, CancellationToken cancellationToken)
+    {
+        var response = await client.InvokeModelAsync(modelId, request, cancellationToken);
+        return response.GetMessageResponse();
+    }
+
+    IAsyncEnumerable<IMessageStreamEvent> IMessages.CreateStreamAsync(MessageRequest request, RequestOptions? overrideOptions, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
+
 public static class BedrockExtensions
 {
+    public static BedrockAnthropicClient UseAnthropic(this AmazonBedrockRuntimeClient client, string modelId)
+    {
+        return new BedrockAnthropicClient(client, modelId);
+    }
+
     public static Task<InvokeModelResponse> InvokeModelAsync(this AmazonBedrockRuntimeClient client, string modelId, MessageRequest request, CancellationToken cancellationToken = default)
     {
         return client.InvokeModelAsync(new Amazon.BedrockRuntime.Model.InvokeModelRequest
@@ -14,7 +36,24 @@ public static class BedrockExtensions
             Accept = "application/json",
             ContentType = "application/json",
             Body = Serialize(request, stream: null),
-        });
+        }, cancellationToken);
+    }
+
+    public static MessageResponse GetMessageResponse(this InvokeModelResponse response)
+    {
+        if ((int)response.HttpStatusCode == 200)
+        {
+            return JsonSerializer.Deserialize<MessageResponse>(response.Body, AnthropicJsonSerialzierContext.Default.Options)!;
+        }
+        else
+        {
+            var shape = JsonSerializer.Deserialize<ErrorResponseShape>(response.Body, AnthropicJsonSerialzierContext.Default.Options)!;
+
+            var error = shape!.ErrorResponse;
+            var errorMsg = error.Message;
+            var code = (ErrorCode)response.HttpStatusCode;
+            throw new ClaudiaException(code, error.Type, errorMsg);
+        }
     }
 
     static MemoryStream Serialize(MessageRequest request, bool? stream)
