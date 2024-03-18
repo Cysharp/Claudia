02e647794874544a18886fd46919f55234bc0129
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2024-03-18 14:13:36 +0900

    feat: add bedrock raw request

---
+++ sandbox/BedrockConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using Amazon;
+using Amazon.BedrockRuntime;
+using Amazon.Util;
+using System.Text.Json.Nodes;
+
+// credentials is your own
+AWSConfigs.AWSProfileName = "<your-profile>";
+
+var bedrock = new AmazonBedrockRuntimeClient(RegionEndpoint.USEast1);
+
+var input = """
+        What time is it in Seattle and Tokyo?
+        Incidentally multiply 1,984,135 by 9,343,116.
+""";
+var payload = new JsonObject()
+    {
+        { "anthropic_version", "bedrock-2023-05-31" },
+        { "max_tokens", 1000 },
+        { "messages", new JsonArray(new JsonObject{
+            { "role", "user" },
+            { "content", new JsonArray(new JsonObject{
+                { "type", "text" },
+                { "text", input }
+            })}
+        })
+        }
+    }.ToJsonString();
+var response = await bedrock.InvokeModelAsync(new Amazon.BedrockRuntime.Model.InvokeModelRequest
+{
+    ModelId = "anthropic.claude-3-sonnet-20240229-v1:0",
+    Accept = "application/json",
+    ContentType = "application/json",
+    Body = AWSSDKUtils.GenerateMemoryStreamFromString(payload),
+});
+
+if (response.HttpStatusCode != System.Net.HttpStatusCode.OK)
+    throw new HttpRequestException("Request Failed", null, response.HttpStatusCode);
+
+var resBody = await JsonNode.ParseAsync(response.Body);
+Console.WriteLine(resBody);
