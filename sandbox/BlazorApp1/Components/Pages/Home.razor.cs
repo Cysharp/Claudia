21548980f9facbdf9bd351ed36242d2e6b0fffa5
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2024-03-25 19:10:37 +0900

    change sandbox blazor to haiku

--- sandbox/BlazorApp1/Components/Pages/Home.razor.cs
+++ sandbox/BlazorApp1/Components/Pages/Home.razor.cs
@@ -29,7 +29,7 @@ public partial class Home
 
             var stream = Anthropic.Messages.CreateStreamAsync(new()
             {
-                Model = Models.Claude3Opus,
+                Model = Models.Claude3Haiku,
                 MaxTokens = 1024,
                 Temperature = temperature,
                 System = string.IsNullOrWhiteSpace(systemInput) ? null : systemInput,
